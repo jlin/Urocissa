68727a364cfb3c423cee57cc993712b83fa6a410
hsa00000 <hsa00000@gmail.com>
2024-10-20 19:21:28 +0800

    remove album's setting from get_img.rs

--- gallery-backend/src/router/get/get_img.rs
+++ gallery-backend/src/router/get/get_img.rs
@@ -17,34 +17,6 @@ pub async fn compressed_file(
     cookies: &CookieJar<'_>,
     file_path: PathBuf,
 ) -> Result<CompressedFileResponse<'static>, Status> {
-    let file_hash = file_path
-        .file_stem()
-        .and_then(std::ffi::OsStr::to_str)
-        .ok_or(Status::BadRequest)?;
-
-    let password_cookie = cookies.get("password");
-    let share_id_cookie = cookies.get("share");
-    let share_id = share_id_cookie.map_or("", |cookie| cookie.value());
-
-    if !password_cookie.map_or(false, |cookie| cookie.value() == PRIVATE_CONFIG.password) {
-        // Handle TREE_DB lookup and early return if any condition fails
-        let read_txn = TREE.in_disk.begin_read().unwrap();
-        let read_table = read_txn.open_table(DATA_TABLE).unwrap();
-        let database = read_table
-            .get(&*file_hash.to_string())
-            .map_err(|_| Status::InternalServerError)?
-            .ok_or(Status::NotFound)?
-            .value();
-        if !database.album.contains(share_id) {
-            println!(
-                "Database album {:?} does not contain share ID {:?}",
-                database.album, share_id
-            );
-            return Err(Status::NotFound);
-        }
-    }
-
-    // Handle file serving outside of the password check and data retrieval block
     let compressed_file_path = Path::new("./object/compressed").join(&file_path);
     if compressed_file_path
         .extension()
