912ac3ddb18a436e9973bdbc8edbcddfe7c03909
hsa00000 <hsa00000@gmail.com>
2024-10-23 19:36:20 +0800

    Revert "Router push to HomePage by name instead of url."
    This reverts commit 60f38e22eca9c6db9248e30de0d7d1fbcf4328a7.

--- gallery-frontend/src/components/LoginPage.vue
+++ gallery-frontend/src/components/LoginPage.vue
@@ -1,120 +1,114 @@
 <template>
-  <div class="login-container">
-    <div class="login">
-      <div class="header">Welcome Back!</div>
-      <form @submit.prevent="handleLogin" class="login-form">
-        <div class="input-group">
-          <input
-            id="password-holder"
-            type="password"
-            v-model="password"
-            placeholder="Password"
-            required
-          />
+    <div class="login-container">
+        <div class="login">
+            <div class="header">Welcome Back!</div>
+            <form @submit.prevent="handleLogin" class="login-form">
+                <div class="input-group">
+                    <input id="password-holder" type="password" v-model="password" placeholder="Password" required />
+                </div>
+                <button type="submit" class="login-button">Login</button>
+            </form>
         </div>
-        <button type="submit" class="login-button">Login</button>
-      </form>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import Cookies from 'js-cookie'
-import axios from 'axios'
-import { useRouter } from 'vue-router'
-import { useInitializedStore } from '@/store/initializedStore'
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 const password = ref('')
-const token = ref<string>('') // To store the JWT token
+const token = ref<string>(''); // To store the JWT token
 const router = useRouter()
-const initializedStore = useInitializedStore()
 const handleLogin = async () => {
-  try {
-    const response = await axios.post('/post/authenticate', password.value, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    token.value = response.data
+    try {
+        const response = await axios.post('/post/authenticate', password.value, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        token.value = response.data;
 
-    // Store the JWT in a cookie with security attributes
-    Cookies.set('jwt', token.value, {
-      httpOnly: false, // Set to true for better security (cannot access via JavaScript)
-      secure: true, // Ensure it's only sent over HTTPS
-      sameSite: 'Strict', // Prevent CSRF attacks
-      expires: 1 // Optional: Expires in 1 day
-    })
-    initializedStore.login = true
-    await router.push({ name: 'AllPage' })
-  } catch (error) {
-    console.error(error)
-  }
-}
+        // Store the JWT in a cookie with security attributes
+        Cookies.set('jwt', token.value, {
+            httpOnly: false,  // Set to true for better security (cannot access via JavaScript)
+            secure: true,      // Ensure it's only sent over HTTPS
+            sameSite: 'Strict', // Prevent CSRF attacks
+            expires: 1,        // Optional: Expires in 1 day
+        });
+
+        await router.push('/')
+        window.location.reload()
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 </script>
 
 <style scoped>
 .login-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #3d3d3d;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #3d3d3d;
 }
 
 .login {
-  max-width: 400px;
-  width: 100%;
-  padding: 40px;
-  background-color: #3d3d3d;
-  border: 1px solid gainsboro;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+    width: 100%;
+    padding: 40px;
+    background-color: #3d3d3d;
+    border: 1px solid gainsboro;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .header {
-  font-size: 24px;
-  margin-bottom: 20px;
-  color: gainsboro;
-  text-align: center;
+    font-size: 24px;
+    margin-bottom: 20px;
+    color: gainsboro;
+    text-align: center;
 }
 
 .login-form {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 .input-group {
-  margin-bottom: 20px;
+    margin-bottom: 20px;
 }
 
 input {
-  box-sizing: border-box;
-  font-size: 18px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  color: gainsboro;
-  background-color: #2c2c2c;
-  width: 100%;
-  padding: 10px;
+    box-sizing: border-box;
+    font-size: 18px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    color: gainsboro;
+    background-color: #2c2c2c;
+    width: 100%;
+    padding: 10px;
 }
 
 input::placeholder {
-  color: #aaa;
+    color: #aaa;
 }
 
 .login-button {
-  padding: 10px;
-  font-size: 18px;
-  background-color: #28a745;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+    padding: 10px;
+    font-size: 18px;
+    background-color: #28a745;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
 }
 
 .login-button:hover {
-  background-color: #218838;
+    background-color: #218838;
 }
 </style>
