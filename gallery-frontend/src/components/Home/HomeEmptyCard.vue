22b2de53b3623053bc85fd31f18b7b875a508190
hsa00000 <hsa00000@gmail.com>
2024-11-08 10:41:57 +0800

    Enhance HomeEmptyCard component with hover effects for improved user interaction

--- gallery-frontend/src/components/Home/HomeEmptyCard.vue
+++ gallery-frontend/src/components/Home/HomeEmptyCard.vue
@@ -1,22 +1,31 @@
 <template>
   <v-container class="d-flex align-center justify-center" fluid>
-    <v-card
-      class="pa-4 text-center mx-auto"
-      elevation="12"
-      max-width="600"
-      rounded="lg"
-      width="100%"
-    >
-      <v-icon class="mb-5" color="grey" size="100"> mdi-image-plus </v-icon>
-      <v-card-item>
-        <v-card-subtitle>
-          Wow, so empty!<br />
-          Try adding some photos here!
-        </v-card-subtitle>
-      </v-card-item>
-    </v-card>
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card
+        class="pa-4 text-center mx-auto hover-cursor"
+        :style="{
+          border: isHovering ? '2px solid #BDBDBD' : '2px solid transparent'
+        }"
+        :elevation="isHovering ? 12 : 2"
+        max-width="600"
+        rounded="lg"
+        width="100%"
+        v-bind="props"
+      >
+        <v-icon class="mb-5" color="grey" size="100"> mdi-image-plus </v-icon>
+        <v-card-item>
+          <v-card-subtitle>
+            Wow, so empty!<br />
+            Try adding some photos here!
+          </v-card-subtitle>
+        </v-card-item>
+      </v-card>
+    </v-hover>
   </v-container>
 </template>
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
+<style scoped>
+.hover-cursor {
+  cursor: pointer !important;
+}
+</style>
