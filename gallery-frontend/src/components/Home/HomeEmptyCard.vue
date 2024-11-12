7f2d6310d1f73fd34e95ca7be1496025d2652eb2
hsa00000 <hsa00000@gmail.com>
2024-11-12 08:48:39 +0800

    enhance: enhance `HomeEmptyCard` with dynamic hover effects and messages based on route

--- gallery-frontend/src/components/Home/HomeEmptyCard.vue
+++ gallery-frontend/src/components/Home/HomeEmptyCard.vue
@@ -2,11 +2,33 @@
   <v-container class="d-flex align-center justify-center" fluid>
     <v-hover v-slot="{ isHovering, props }">
       <v-card
+        v-if="hasHoveringEffect"
         class="pa-4 text-center mx-auto hover-cursor"
         :style="{
-          border: isHovering ? '2px solid #BDBDBD' : '2px solid transparent'
+          border: isHovering && hasHoveringEffect ? '2px solid #BDBDBD' : '2px solid transparent'
         }"
-        :elevation="isHovering ? 12 : 2"
+        :elevation="isHovering && hasHoveringEffect ? 12 : 2"
+        max-width="600"
+        rounded="lg"
+        width="100%"
+        v-bind="props"
+        @click="clickEmptyCard()"
+      >
+        <v-icon class="mb-5" color="grey" size="100"> mdi-image-plus </v-icon>
+        <v-card-item>
+          <v-card-subtitle>
+            Wow, so empty!<br />
+            {{ computedMessage }}
+          </v-card-subtitle>
+        </v-card-item>
+      </v-card>
+      <v-card
+        v-else
+        class="pa-4 text-center mx-auto"
+        :style="{
+          border: '2px solid transparent'
+        }"
+        :elevation="2"
         max-width="600"
         rounded="lg"
         width="100%"
@@ -24,9 +46,29 @@
   </v-container>
 </template>
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
+
+const hasHoveringEffect = computed(() => {
+  const path = route.path
+
+  if (path.startsWith('/favorite')) {
+    return false
+  } else if (path.startsWith('/archived')) {
+    return false
+  } else if (path.startsWith('/trashed')) {
+    return false
+  } else if (path.startsWith('/albums')) {
+    return true
+  } else if (path.startsWith('/all')) {
+    return true
+  } else if (path.startsWith('/album-')) {
+    return true
+  } else {
+    return true
+  }
+})
 
 const computedMessage = computed(() => {
   const path = route.path
@@ -41,12 +83,14 @@ const computedMessage = computed(() => {
     return 'The album feature is still under development!'
   } else if (path.startsWith('/all')) {
     return 'Try uploading some photos here!'
-  } else if (path.startsWith('/album-') && path.includes('/')) {
+  } else if (path.startsWith('/album-')) {
     return 'Add some photos to this album!'
   } else {
     return 'Try uploading some photos here!'
   }
 })
+
+const clickEmptyCard = () => {}
 </script>
 <style scoped>
 .hover-cursor {
