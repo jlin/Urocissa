51375b992b408163964070770c4f83fa3f83e35b
hsa00000 <hsa00000@gmail.com>
2024-11-18 11:04:13 +0800

    fix: remove unnecessary Vue module declaration in vue-shim.d.ts

--- gallery-frontend/src/script/vue-shim.d.ts
+++ gallery-frontend/src/script/vue-shim.d.ts
@@ -1,9 +1,3 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare module '*/dataWorker?worker&inline' {
   const workerConstructor: new () => Worker
   export default workerConstructor
