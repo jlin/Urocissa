acac31e191d3e93f5318778ce95fa84ae0cc2ac0
hsa00000 <hsa00000@gmail.com>
2024-11-18 18:55:38 +0800

    feat: add TypeScript declaration for Vue components to enhance type safety

--- gallery-frontend/src/script/vue-shim.d.ts
+++ gallery-frontend/src/script/vue-shim.d.ts
@@ -1,3 +1,10 @@
+// vue-shim.d.ts
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<unknown, unknown, unknown>
+  export default component
+}
+
 declare module '*/dataWorker?worker&inline' {
   const workerConstructor: new () => Worker
   export default workerConstructor
