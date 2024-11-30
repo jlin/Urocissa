b364f24828f4de47dd0429677f52b1f81581d669
hsa00000 <hsa00000@gmail.com>
2024-11-30 10:38:01 +0800

    refactor: optimize event handling by reducing unnecessary batch creation and improving deduplication logic

--- gallery-backend/src/synchronizer/event.rs
+++ gallery-backend/src/synchronizer/event.rs
@@ -14,17 +14,13 @@ pub fn start_event_channel() -> tokio::task::JoinHandle<()> {
     EVENTS_SENDER.set(events_sender).unwrap();
 
     tokio::task::spawn(async move {
-        while let Some(list_of_sync_files) = events_receiver.recv().await {
-            // Initialize a batch with the received list
-            let mut batch = Vec::with_capacity(BATCH_SIZE);
-            batch.extend(list_of_sync_files);
-
+        while let Some(mut list_of_sync_files) = events_receiver.recv().await {
             // Attempt to drain additional items without waiting
             let start_time = std::time::Instant::now();
-            while batch.len() < BATCH_SIZE {
+            while list_of_sync_files.len() < BATCH_SIZE {
                 match events_receiver.try_recv() {
                     Ok(mut more_files) => {
-                        batch.append(&mut more_files);
+                        list_of_sync_files.append(&mut more_files);
                     }
                     Err(mpsc::error::TryRecvError::Empty) => {
                         // No more items are immediately available
@@ -38,14 +34,11 @@ pub fn start_event_channel() -> tokio::task::JoinHandle<()> {
             }
             info!(duration = &*format!("{:?}", start_time.elapsed()); "received events");
 
-            // Deduplicate the paths
-            let unique_paths: HashSet<PathBuf> = batch.into_iter().collect();
-            let paths: Vec<PathBuf> = unique_paths.into_iter().collect();
-
-            // Spawn a blocking task to handle the batch
-            let paths_clone = paths.clone();
             tokio::task::spawn_blocking(move || {
-                executor(paths_clone);
+                // Deduplicate the paths
+                let unique_paths: HashSet<PathBuf> = list_of_sync_files.into_iter().collect();
+                let paths: Vec<PathBuf> = unique_paths.into_iter().collect();
+                executor(paths);
             })
             .await
             .unwrap();
