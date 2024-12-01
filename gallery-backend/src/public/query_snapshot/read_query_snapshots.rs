ce92f2897fbdf0e9e399ad113cace3e9c7d059b1
hsa00000 <hsa00000@gmail.com>
2024-12-01 16:21:02 +0800

    refactor: change error handling in read_query_snapshot to use Box<dyn Error>

--- gallery-backend/src/public/query_snapshot/read_query_snapshots.rs
+++ gallery-backend/src/public/query_snapshot/read_query_snapshots.rs
@@ -1,39 +1,29 @@
-use std::sync::atomic::Ordering;
+use std::{error::Error, sync::atomic::Ordering};
 
 use super::QuerySnapshot;
 use crate::public::{query_snapshot::PrefetchReturn, tree::start_loop::VERSION_COUNT};
 
 use redb::TableDefinition;
-use rocket::http::Status;
 
 impl QuerySnapshot {
     pub fn read_query_snapshot(
         &'static self,
         query_hash: u64,
-    ) -> Result<Option<PrefetchReturn>, Status> {
+    ) -> Result<Option<PrefetchReturn>, Box<dyn Error>> {
         if let Some(data) = self.in_memory.get(&query_hash) {
             return Ok(Some(data.value().clone()));
         }
 
-        let read_txn = self.in_disk.begin_read().map_err(|err| {
-            error!("{:?}", err);
-            Status::InternalServerError
-        })?;
+        let read_txn = self.in_disk.begin_read().unwrap();
 
         let count_version = &VERSION_COUNT.load(Ordering::Relaxed).to_string();
 
         let table_definition: TableDefinition<u64, PrefetchReturn> =
             TableDefinition::new(&count_version);
 
-        let table = read_txn.open_table(table_definition).map_err(|err| {
-            error!("{:?}", err);
-            Status::InternalServerError
-        })?;
+        let table = read_txn.open_table(table_definition)?;
 
-        let timestamp = table.get(query_hash).map_err(|err| {
-            error!("{:?}", err);
-            Status::InternalServerError
-        })?;
+        let timestamp = table.get(query_hash)?;
 
         Ok(timestamp.map(|inner_value| inner_value.value()))
     }
