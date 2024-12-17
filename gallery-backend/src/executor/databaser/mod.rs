5459c339ebf00360eb8445af4c87fff2948ffbb7
hsa00000 <hsa00000@gmail.com>
2024-12-17 16:21:20 +0800

    refactor: optimize databaser function by using filter_map for processing images and videos

--- gallery-backend/src/executor/databaser/mod.rs
+++ gallery-backend/src/executor/databaser/mod.rs
@@ -30,46 +30,53 @@ pub mod video_compressor;
 pub mod video_ffprobe;
 pub fn databaser(vec_of_hash_alias: DashMap<ArrayString<64>, DataBase>) -> () {
     let write_txn = TREE.in_disk.begin_write().unwrap();
-    let mut write_table = write_txn.open_table(DATA_TABLE).unwrap();
     let video_hash_dashset = DashSet::new();
-    vec_of_hash_alias
-        .into_par_iter()
-        .for_each(move |(hash, mut database)| {
-            if VALID_IMAGE_EXTENSIONS.contains(&database.ext.as_str()) {
-                match process_image_info(&mut database) {
-                    Ok(_) => {
-                        write_table.insert(&*database.hash, &database).unwrap();
+    {
+        let mut write_table = write_txn.open_table(DATA_TABLE).unwrap();
+
+        let vec: Vec<_> = vec_of_hash_alias
+            .into_par_iter()
+            .filter_map(|(hash, mut database)| {
+                if VALID_IMAGE_EXTENSIONS.contains(&database.ext.as_str()) {
+                    match process_image_info(&mut database) {
+                        Ok(_) => Some(database),
+                        Err(e) => {
+                            handle_error(ErrorData::new(
+                                e.to_string(),
+                                format!("An error occurred while processing file",),
+                                Some(database.hash),
+                                Some(database.source_path()),
+                                Location::caller(),
+                                Some(database),
+                            ));
+                            None
+                        }
                     }
-                    Err(e) => {
-                        handle_error(ErrorData::new(
-                            e.to_string(),
-                            format!("An error occurred while processing file",),
-                            Some(database.hash),
-                            Some(database.source_path()),
-                            Location::caller(),
-                            Some(database),
-                        ));
+                } else {
+                    match process_video_info(&mut database) {
+                        Ok(_) => {
+                            video_hash_dashset.insert(database.hash);
+                            Some(database)
+                        }
+                        Err(e) => {
+                            handle_error(ErrorData::new(
+                                e.to_string(),
+                                format!("An error occurred while processing file",),
+                                Some(database.hash),
+                                Some(database.source_path()),
+                                Location::caller(),
+                                Some(database),
+                            ));
+                            None
+                        }
                     }
                 }
-            } else {
-                match process_video_info(&mut database) {
-                    Ok(_) => {
-                        video_hash_dashset.insert(database.hash);
-                        write_table.insert(&*database.hash, &database).unwrap();
-                    }
-                    Err(e) => {
-                        handle_error(ErrorData::new(
-                            e.to_string(),
-                            format!("An error occurred while processing file",),
-                            Some(database.hash),
-                            Some(database.source_path()),
-                            Location::caller(),
-                            Some(database),
-                        ));
-                    }
-                }
-            }
+            })
+            .collect();
+        vec.iter().for_each(|database| {
+            write_table.insert(&*database.hash, database).unwrap();
         });
+    }
     write_txn.commit().unwrap();
     // Send video hashes to the worker thread
     VIDEO_QUEUE_SENDER
