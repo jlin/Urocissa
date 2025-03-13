999aac066455566b251343c5155f5dcc5c42c296
hsa00000 <hsa00000@gmail.com>
2025-03-13 15:56:14 +0800

    fix: correct the number of valid video extensions in constants

--- gallery-backend/src/public/constant.rs
+++ gallery-backend/src/public/constant.rs
@@ -7,8 +7,9 @@ pub const SHOULD_SWAP_WIDTH_HEIGHT_ROTATION: [&str; 4] = ["90", "-90", "270", "-
 pub const VALID_IMAGE_EXTENSIONS: [&str; 9] = [
     "jpg", "jpeg", "jfif", "jpe", "png", "tif", "tiff", "webp", "bmp",
 ];
-pub const VALID_VIDEO_EXTENSIONS: [&str; 10] = [
-    "gif", "mp4", "webm", "mkv", "mov", "avi", "mkv", "flv", "wmv", "mpeg",
+
+pub const VALID_VIDEO_EXTENSIONS: [&str; 9] = [
+    "gif", "mp4", "webm", "mkv", "mov", "avi", "flv", "wmv", "mpeg",
 ];
 
 pub const DEFAULT_PRIORITY_LIST: [&str; 4] =
