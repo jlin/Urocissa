124eb8801a4d7f63f46d1a4c0f485cebb8e331d1
hsa00000 <hsa00000@gmail.com>
2025-03-13 15:58:44 +0800

    refactor: update constant definitions to use static lifetimes

--- gallery-backend/src/public/constant.rs
+++ gallery-backend/src/public/constant.rs
@@ -2,15 +2,16 @@ pub const ROW_BATCH_NUMBER: usize = 20;
 
 pub const PROCESS_BATCH_NUMBER: usize = 100;
 
-pub const SHOULD_SWAP_WIDTH_HEIGHT_ROTATION: [&str; 4] = ["90", "-90", "270", "-270"];
+pub const SHOULD_SWAP_WIDTH_HEIGHT_ROTATION: &'static [&'static str] =
+    &["90", "-90", "270", "-270"];
 
-pub const VALID_IMAGE_EXTENSIONS: [&str; 9] = [
+pub const VALID_IMAGE_EXTENSIONS: &'static [&'static str] = &[
     "jpg", "jpeg", "jfif", "jpe", "png", "tif", "tiff", "webp", "bmp",
 ];
 
-pub const VALID_VIDEO_EXTENSIONS: [&str; 9] = [
+pub const VALID_VIDEO_EXTENSIONS: &'static [&'static str] = &[
     "gif", "mp4", "webm", "mkv", "mov", "avi", "flv", "wmv", "mpeg",
 ];
 
-pub const DEFAULT_PRIORITY_LIST: [&str; 4] =
-    ["DateTimeOriginal", "filename", "modified", "scan_time"];
+pub const DEFAULT_PRIORITY_LIST: &'static [&'static str] =
+    &["DateTimeOriginal", "filename", "modified", "scan_time"];
