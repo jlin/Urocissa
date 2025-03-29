a0e4eec0696fecae8fb4c655dd4b155bedbb188f
hsa00000 <hsa00000@gmail.com>
2025-03-29 09:34:20 +0800

    refactor: remove InsideAlbumPage and InsideSharePage components as they are no longer needed

--- gallery-frontend/src/components/Page/InsideSharePage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <HomePage />
-</template>
-
-<script setup lang="ts">
-import HomeMain from '@/components/Home/Page/HomeMain.vue'
-defineProps<{ id: string; hash?: string }>()
-</script>
