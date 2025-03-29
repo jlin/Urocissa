aa18d0b1f47a9c40d87c143ce33ba90e376d98ef
hsa00000 <hsa00000@gmail.com>
2025-03-29 08:50:17 +0800

    feat: add search functionality to ShareBar component with routing support

--- gallery-frontend/src/components/NavBar/ShareBar.vue
+++ gallery-frontend/src/components/NavBar/ShareBar.vue
@@ -14,8 +14,39 @@
       backgroundColor: '#212121'
     }"
   >
-    <v-card elevation="0" class="w-100">
+    <v-card elevation="0" class="w-50">
       <v-card-title> test </v-card-title>
+    </v-card>
+    <v-card
+      elevation="0"
+      :style="{
+        width: '50%'
+      }"
+    >
+      <v-card-text class="pa-0">
+        <v-text-field
+          id="nav-search-input"
+          rounded
+          class="ma-0"
+          v-model="searchQuery"
+          bg-color="grey-darken-2"
+          @click:prepend-inner="handleSearch"
+          @click:clear="handleSearch"
+          @keyup.enter="handleSearch"
+          clearable
+          persistent-clear
+          variant="solo"
+          flat
+          prepend-inner-icon="mdi-magnify"
+          single-line
+          hide-details
+          style="margin-right: 10px"
+        >
+          <template #label>
+            <span class="text-caption">Search</span>
+          </template>
+        </v-text-field>
+      </v-card-text>
     </v-card>
     <v-spacer></v-spacer>
   </v-toolbar>
@@ -23,4 +54,25 @@
 </template>
 <script setup lang="ts">
 import ProgessBar from '@/components/NavBar/ProgessBar.vue'
+import { Ref, ref, watchEffect } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useFilterStore } from '@/store/filterStore'
+
+const filterStore = useFilterStore('mainId')
+
+const route = useRoute()
+const router = useRouter()
+const searchQuery: Ref<string | null> = ref(null)
+
+const handleSearch = async () => {
+  filterStore.searchString = searchQuery.value
+  await router.replace({
+    path: route.path,
+    query: { search: searchQuery.value }
+  })
+}
+
+watchEffect(() => {
+  searchQuery.value = filterStore.searchString
+})
 </script>
