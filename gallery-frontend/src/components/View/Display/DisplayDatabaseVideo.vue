0798e1ab297b1925665c5fb5ca8641f1c3b38990
hsa00000 <hsa00000@gmail.com>
2025-06-02 15:07:42 +0800

    feat: add token readiness check for video component

--- gallery-frontend/src/components/View/Display/DisplayDatabaseVideo.vue
+++ gallery-frontend/src/components/View/Display/DisplayDatabaseVideo.vue
@@ -1,5 +1,6 @@
 <template>
   <video
+    v-if="tokenReady"
     controls
     autoplay
     :src="getSrc(hash, false, 'mp4', Cookies.get('jwt')!, undefined)"
@@ -21,18 +22,31 @@
 import { Database, IsolationId } from '@type/types'
 import Cookies from 'js-cookie'
 import { useCurrentFrameStore } from '@/store/currentFrameStore'
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { getSrc } from '@/../config'
+import { useTokenStore } from '@/store/tokenStore'
+import { storeHashToken } from '@/db/db'
 const props = defineProps<{
   isolationId: IsolationId
   hash: string
   database: Database
 }>()
 
+const tokenReady = ref(false)
+
+const tokenStore = useTokenStore(props.isolationId)
 const currentFrameStore = useCurrentFrameStore(props.isolationId)
+
 const videoRef = ref<HTMLVideoElement | null>(null)
 
 watch(videoRef, () => {
   currentFrameStore.video = videoRef.value
+})
+onMounted(async () => {
+  const token = tokenStore.hashTokenMap.get(props.database.hash)
+  if (token !== undefined) {
+    await storeHashToken(props.database.hash, token)
+    tokenReady.value = true
+  }
 })
 </script>
