c478101e300eace899c33298d044ad7d445cb99a
ZHI LIN <hsa00000@gmail.com>
2025-09-29 09:41:22 +0800

    style(ViewPageMetadata): enhance background color consistency and remove inline styles

--- gallery-frontend/src/components/View/Metadata/ViewPageMetadata.vue
+++ gallery-frontend/src/components/View/Metadata/ViewPageMetadata.vue
@@ -2,27 +2,23 @@
   <div
     id="abstractData-col"
     v-if="abstractData"
-    class="h-100 flex-grow-0 flex-shrink-0"
+    class="h-100 flex-grow-0 flex-shrink-0 bg-surface"
     :style="{
-      backgroundColor: '#121212',
       width: constStore.showInfo ? '360px' : '0',
       zIndex: 1
     }"
   >
     <div class="position-relative">
       <div>
-        <v-toolbar
-          color="#121212"
-          theme="dark"
-        >
+        <v-toolbar>
           <v-btn icon @click="toggleInfo">
-            <v-icon color="#e0e0e0">mdi-close</v-icon>
+            <v-icon>mdi-close</v-icon>
           </v-btn>
-          <v-toolbar-title class="text-h5" style="color: #e0e0e0;">Info</v-toolbar-title>
+          <v-toolbar-title class="text-h5">Info</v-toolbar-title>
         </v-toolbar>
       </div>
       <div v-if="abstractData.database" class="h-100 w-100">
-        <v-list bg-color="#121212" theme="dark" class="pa-0" height="100%" lines="two">
+        <v-list class="pa-0" height="100%" lines="two">
           <ItemSize :database="abstractData.database" />
           <ItemPath v-if="showMetadata" :database="abstractData.database" />
           <ItemDate :database="abstractData.database" />
@@ -49,7 +45,7 @@
         </v-list>
       </div>
       <div v-if="abstractData.album" class="h-100 w-100">
-        <v-list bg-color="#121212" theme="dark" class="pa-0" height="100%" lines="two">
+        <v-list theme="dark" class="pa-0" height="100%" lines="two">
           <ItemTitle :title="abstractData.album.title" />
           <ItemCount :album="abstractData.album" />
           <v-divider></v-divider>
